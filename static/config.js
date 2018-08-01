fb8cd5e6c26c7445ed0e364e1d1fb397834f54d5
lixin <1411341896@qq.com>
2018-08-01 09:29:07 +0800

    机电学院20180801

---
+++ static/config.js
@@ -0,0 +1,11 @@
+const config = {
+	// url: 'http://192.168.0.56:8086/enroll/'
+	// url: 'http://192.168.0.56:80/enroll/'
+	url: 'http://222.179.45.246:8083/enroll/'//机电学院
+	// url: 'http://222.179.134.204:8081/enroll/'//移通学院
+	// url: 'http://192.168.0.140:8086/enroll/'
+	// url: 'http://zsyx.ctbu.edu.cn:80/enroll/'
+	// url:'http://www.yunhuakeji.com:10071/enroll/',
+	// url: 'http://202.202.144.153:8082/enroll/'
+	//url: 'http://yx.cqut.edu.cn/enroll/'
+}
